c62d28b6df9dd6e120755e25fba9debcd408f5b3
swortal <swortal@gmail.com>
2025-04-15 03:19:22 -0700

    schema definition for news summary app

--- backend/db/models.py
+++ backend/db/models.py
@@ -1,20 +1,146 @@
-from sqlalchemy import Column, Integer, String, DateTime, Enum
+from sqlalchemy import Column, Integer, String, DateTime, Enum, TypeDecorator, Date
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.postgresql import JSONB, ARRAY
 from datetime import datetime
 import enum
+from pydantic import BaseModel
+import json
 
 Base = declarative_base()
+
 
 class UserStatus(enum.Enum):
     pending = "pending"
     active = "active"
 
+
+class ChatMessageGeneratorRole(enum.Enum):
+    user = "user"
+    system = "system"
+
+
+class ConversationHistoryItem(BaseModel):
+    role: ChatMessageGeneratorRole
+    content: str
+
+
+class ConversationHistory(BaseModel):
+    messages: list[ConversationHistoryItem]
+    thread_id: str
+
+
+class ConversationHistoryPostGreSqlWrapper(TypeDecorator):
+    impl = JSONB
+    cache_ok = True
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            if isinstance(value, ConversationHistory):
+                return json.loads(value.model_dump_json())
+            return value
+        return None
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            return ConversationHistory(**value)
+        return None
+
+
 class User(Base):
     __tablename__ = "users"
-    
+
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     email = Column(String, unique=True, index=True)
     name = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    created_at = Column(DateTime, default=datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.now(datetime.UTC))
     status = Column(Enum(UserStatus), default=UserStatus.pending)
+    news_preference = Column(String)
+    current_news_preference_version_id = Column(Integer)
+    subscribed_rss_feeds_id = Column(ARRAY(Integer))
+
+
+class RssFeed(Base):
+    __tablename__ = "rss_feeds"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    feed_url = Column(String, unique=True, index=True)
+    last_crawl_time = Column(DateTime, default=datetime.now(datetime.UTC))
+    title = Column(String)
+    html_url = Column(String)
+    xml_url = Column(String)
+
+
+class NewsEntry(Base):
+    __tablename__ = "news_entries"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    rss_feed_id = Column(Integer)
+    entry_rss_guid = Column(String, unique=True, index=True)
+    entry_url = Column(String)
+    crawl_time = Column(DateTime, default=datetime.now(datetime.UTC))
+    title = Column(String)
+    description = Column(String)
+    content = Column(String)
+
+
+class NewsSummaryItem(BaseModel):
+    title: str
+    content: str
+    reference_urls: list[str]
+    clicked: bool
+
+
+# list of news summary items. sorted by ranking from high to low
+class NewsSummaryList(BaseModel):
+    summary: list[NewsSummaryItem]
+
+
+class NewsSummaryListPostGreSqlWrapper(TypeDecorator):
+    impl = JSONB
+    cache_ok = True
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            if isinstance(value, NewsSummaryList):
+                return json.loads(value.model_dump_json())
+            return value
+        return None
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            return NewsSummaryList(**value)
+        return None
+
+
+class NewsSummary(Base):
+    __tablename__ = "news_summaries"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(Integer)
+    start_date = Column(Date)
+    end_date = Column(Date)
+    content = Column(NewsSummaryListPostGreSqlWrapper)
+
+
+class NewsPreferenceChangeCause(enum.Enum):
+    survey = "survey"
+    user_edit = "user_edit"
+    news_click = "news_click"
+
+
+class NewsPreferenceVersion(Base):
+    __tablename__ = "news_preference_versions"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(Integer)
+    previous_version_id = Column(Integer)  # -1 if no previous version
+    content = Column(String)
+    cause = Column(Enum(NewsPreferenceChangeCause))
+    causal_survey_conversation_history = Column(
+        ConversationHistoryPostGreSqlWrapper
+    )  # survey conversation history which caused the change. empty if no change.
+    causal_clicked_news_summary = Column(
+        NewsSummaryListPostGreSqlWrapper
+    )  # clicked news summary which caused the change. empty if no change.
+    created_at = Column(DateTime, default=datetime.now(datetime.UTC))
