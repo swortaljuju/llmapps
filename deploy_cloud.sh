1eeaea5ded0ddaf3c4a8af6da821260d3562366c
swortal <swortal@gmail.com>
2025-06-16 14:43:58 -0700

    add cloud deploy script

---
+++ deploy_cloud.sh
@@ -0,0 +1,7 @@
+git fetch origin
+git rebase origin/main
+npm install
+conda env update --file environment.yml --prune
+npm run build
+# supervisorctl update if you have changed the config files
+sudo supervisorctl restart llmapps:*
